a05b720bdd232847e518fb594aa5f708c2f538ba
chenshu <ranaics@163com>
2016-11-18 17:36:57 +0800

    1

--- application/models/type/type_model.php
+++ application/models/type/type_model.php
@@ -6,9 +6,20 @@
         {
             parent::__construct();
         }
-        /**
-         * 获取分类集合
-         */
 
+        /**
+         * 获取 问题类别信息列表
+         * @param $data
+         * @param array $field
+         * @return array
+         */
+        public function get_info($type_id,$field = array())
+        {
+            $config = array('id','pid');
+            $field = array_merge($config,$field);
+            $infinite = $this -> get_infinite($this->table,$field,'',$type_id);
+            $infinite[] = $data['type'];
+            return $infinite;
+        }
 
     }
